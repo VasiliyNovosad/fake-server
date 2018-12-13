1ec3c7a8afe1256ec4f0f76dc30598b0c5e55a6a
vnovosad <vnovosad@corevalue.net>
2018-12-13 17:24:16 +0200

    added route and data generating for search view

--- routes/api.js
+++ routes/api.js
@@ -6,9 +6,11 @@ const PDFDocument = require('pdfkit');
 
 const generateChats = (count = 5) => {
   return new Promise((resolve, reject) => {
+    const chatTypes = ['Inbox', 'My Research Chat'];
+    const chatNames = ['Macro', 'Risk', 'ESG', 'Research Notes', 'Controversies', 'Base', 'Top Pick', 'Deal', 'Administrative'];
     let chats = [];
     for (let id = 1; id <= count; id++) {
-      chats.push({ id: id, name: 'Investment Conversation', title: faker.commerce.department() });
+      chats.push({ id: id, name: chatNames[faker.random.number({ min: 0, max: chatNames.length - 1 })], type: chatTypes[faker.random.number({ min: 0, max: 1 })] });
     }
     fs.writeFile('./public/jsons/chats.json', JSON.stringify(chats), err => {
       if (err) {
@@ -20,10 +22,10 @@ const generateChats = (count = 5) => {
   });
 };
 
-const generatePDFDocument = (messageId, messageBody, fileName = 'full_note') => {
+const generatePDFDocument = (messageId, messageBody, fileName = 'FullNote') => {
   try {
     const doc = new PDFDocument();
-    let filename = `${messageId}_${fileName}`;
+    let filename = `${messageId}${fileName}`;
     filename = encodeURIComponent(filename) + '.pdf';
     doc.pipe(fs.createWriteStream('./public/pdfs/' + filename));
     doc.text(messageBody, 100, 100);
@@ -43,7 +45,7 @@ const generateMessages = () => {
       const attachment = generatePDFDocument(id, messageBody);
       messages.push({
         id: id,
-        chat_id: Math.floor(Math.random() * 5) + 1,
+        chat_id: faker.random.number({ max: 5 }),
         title: faker.lorem.sentence(),
         body: messageBody,
         created_at: faker.date.recent(1),
@@ -62,6 +64,40 @@ const generateMessages = () => {
         reject(err);
       }
       resolve(messages);
+    });
+  });
+};
+
+const generateSearchResults = () => {
+  return new Promise((resolve, reject) => {
+    let results = [];
+    for (let id = 1; id <= 37; id++) {
+      results.push({
+        id: id,
+        published_at: faker.date.recent(1),
+        entity: faker.company.companyName(),
+        fil_ticker: faker.company.companySuffix(),
+        author: faker.name.firstName() + ' ' + faker.name.lastName(),
+        rating_f: faker.random.number({ max: 9 }),
+        rating_p: faker.random.number({ max: 9 }),
+        title: faker.lorem.sentence(),
+        sector: faker.commerce.department(),
+        esg: faker.random.boolean(),
+        advisor: faker.hacker.abbreviation(),
+        note_type: faker.company.catchPhraseDescriptor(),
+        team: 'Team ' + faker.company.catchPhrase(),
+        business: faker.address.city(),
+        initiation: faker.random.boolean(),
+        publish: faker.address.city(),
+        comments_count: faker.random.number({ max: 20 })
+      });
+    }
+    fs.writeFile('./public/jsons/searchResults.json', JSON.stringify(results), err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
     });
   });
 };
@@ -110,6 +146,28 @@ const getMessages = () => {
   });
 };
 
+const getSearchResults = () => {
+  return new Promise((resolve, reject) => {
+    fs.open('./public/jsons/searchResults.json', 'r', (err, fd) => {
+      if (err) {
+        generateSearchResults().then(searchResults => {
+          resolve(searchResults);
+        }).catch(err => {
+          reject(err);
+        })
+      } else {
+        fs.readFile('./public/jsons/searchResults.json', (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(JSON.parse(data));
+          }
+        });
+      }
+    });
+  });
+};
+
 router.get('/chats', function (req, res) {
   getChats().then(chats => {
     res.json({ status: 'ok', chats: chats });
@@ -139,6 +197,18 @@ router.get('/chats/:chat_id/messages/:id', function (req, res) {
   }).catch(err => {
     res.json({ status: 'error', message: {}, error: err.message });
   });
+});
+
+router.get('/search', function (req, res) {
+  getSearchResults().then(results => {
+    res.json({ status: 'ok', data: results });
+  }).catch(err => {
+    res.json({ status: 'error', data: {}, error: err.message });
+  });
+});
+
+router.get('/messages', function (req, res) {
+  res.json({ status: 'ok', messages_count: faker.random.number({ min: 200, max: 250 }) });
 });
 
 module.exports = router;
